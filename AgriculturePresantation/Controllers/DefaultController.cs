0facc2875e9122d45968af78f17f0dc643b93ebb
aleyna çelik <celikaleyna71@gmail.com>
2022-09-08 15:59:17 +0300

    Manager Newleyerek Listeleme

---
+++ AgriculturePresantation/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresantation.Controllers
+{
+    public class DefaultController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = serviceManager.GetListAll();
+
+            return View(values);
+        }
+    }
+}
