b17fd69bcce9224bc406b03ca7cea366b385ebc0
aleyna çelik <celikaleyna71@gmail.com>
2022-09-08 16:23:37 +0300

    Service Constructor

---
+++ AgriculturePresantation/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresantation.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IServiceService _serviceService;
+
+        public TestController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceService.GetListAll();
+            return View(values);
+        }
+    }
+}
