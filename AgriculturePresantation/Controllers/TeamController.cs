6d29dd78347af6f00f28e6658d677fe9b118689a
aleyna çelik <celikaleyna71@gmail.com>
2022-09-21 12:06:32 +0300

    Team Index

---
+++ AgriculturePresantation/Controllers/TeamController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresantation.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ITeamService _teamService;
+
+        public TeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        public IActionResult Index()
+        {
+
+            var values = _teamService.GetListAll();
+            return View(values);
+        }
+    }
+}
