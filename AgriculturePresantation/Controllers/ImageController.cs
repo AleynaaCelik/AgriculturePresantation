472e527cda205d90b860f210abf94f7550f9bc19
aleyna çelik <celikaleyna71@gmail.com>
2022-09-30 16:16:15 +0300

    Image Listesi

---
+++ AgriculturePresantation/Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresantation.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly IImageService _imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _imageService.GetListAll();
+            return View(values);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/ImageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageManager : IImageService
+    {
+       private readonly IImageDal _imageDal;
+
+        public ImageManager(IImageDal ımageDal)
+        {
+            _imageDal = ımageDal;
+        }
+
+        public void Delete(Image t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Image GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Image> GetListAll()
+        {
+            return _imageDal.GetListAll();
+        }
+
+        public void Insert(Image t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Image t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
