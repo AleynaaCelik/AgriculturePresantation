2dcef57697a7249a365002742b8d908b45942582
aleyna çelik <celikaleyna71@gmail.com>
2022-09-30 13:42:48 +0300

    Announcement Kısmına Giriş

---
+++ AgriculturePresantation/Controllers/AnnouncementController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresantation.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        private readonly IAnnouncementService _announcementService;
+
+        public AnnouncementController(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _announcementService.GetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddAnnouncement()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAnnouncement(Announcement announcement)
+        {
+            _announcementService.Insert(announcement);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteAnnouncement(int id)
+        {
+
+            var values = _announcementService.GetById(id);
+            _announcementService.Delete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditAnnouncement(int id)
+        {
+            var values = _announcementService.GetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EdiAnnouncement(Announcement announcement)
+        {
+            _announcementService.Update(announcement);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
